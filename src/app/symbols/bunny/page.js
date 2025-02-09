4b059b39bea3b6c47bc445a6651e1dc88d565522
Roger Tan <teochewthunder@gmail.com>
2025-02-09 16:53:32 +0800

    Starter sub-page

--- src/app/symbols/bunny/page.js
+++ src/app/symbols/bunny/page.js
@@ -1,17 +1,23 @@
 import Image from "next/image";
 import styles from "../../page.module.css";
 
-export default function Home() {
+export default function SymbolBunny() {
   return (
     <div>
         <Image
-          className="{styles.symbolImage}"
-          src={null}
+          className={styles.symbolImage}
+          src="/symbol_bunny.jpg"
           alt="Bunny"
+          width={300}
+          height={300}
           align="left"
         />
 
-        <p>Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo.</p>
+        <p>Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo. Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo. Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo. Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo. Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo.</p>
+        <p>Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo. Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo. Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo. Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo. Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo.</p>
+        <p>Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo. Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo. Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo. Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo. Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo.</p>
+        <p>Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo. Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo. Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo. Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo. Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo.</p>
+        <p>Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo. Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo. Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo. Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo. Lorem isum isco trunelu fir esta gui norlio mis ut wroiltillo.</p>
     </div>
   );
 }
