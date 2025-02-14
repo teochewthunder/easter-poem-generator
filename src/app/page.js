152a13d45dff01f8050d3f0a84c653ef5a0d401d
Roger Tan <teochewthunder@gmail.com>
2025-02-14 21:49:17 +0800

    Handle Form

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,55 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+import { useState } from 'react';
+
 export default function Home() {
+  const [formData, setFormData] = useState({
+    symbol_bunny: "bunny",
+    symbol_chicks: "",
+    symbol_cross: "",
+    symbol_eggs: "",
+    symbol_lillies: ""
+  });
+
+  const handleChecking = (e) => {
+    const { name, value, checked } = e.target;
+    var finalValue = (checked ? value : "");
+
+    var tempFormData = JSON.stringify(formData);
+    tempFormData = JSON.parse(tempFormData);
+    tempFormData[name] = finalValue;
+
+    if (tempFormData.symbol_bunny + tempFormData.symbol_chicks + tempFormData.symbol_cross + tempFormData.symbol_eggs + tempFormData.symbol_lillies === "") {
+      finalValue = value;
+    }
+
+    setFormData({
+      ...formData,
+      [name]: finalValue
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch('/api/contact', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    });
+    const result = await response.json();
+    console.log(result);
+  };
+
   return (
-    <div>Test Form</div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Bunny:
+        <input type="checkbox" name="symbol_bunny" value="bunny" onClick={handleChecking} />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
   );
 }
