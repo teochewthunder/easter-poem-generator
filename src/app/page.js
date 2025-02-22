c806175ee44312254b5f3b2937cfc3bd81fc6ae0
Roger Tan <teochewthunder@gmail.com>
2025-02-22 08:59:22 +0800

    Styling

--- src/app/page.js
+++ src/app/page.js
@@ -59,38 +59,40 @@ export default function Home() {
   return (
     <div className={styles.generationContainer} >
       <form onSubmit={handleSubmit}>
-        <h1>Generate a Poem!</h1>
-        <label>
+        <h1>Generate an Easter Poem!</h1>
+        <b>Include at least one of these elements</b>
+        <label className={styles.label}>
           <input type="checkbox" name="symbol_bunny" value="bunny" onChange={handleChecking} checked={ (formData.symbol_bunny === "bunny") } />
           &nbsp;Bunny
           &nbsp;<a href="/symbols/bunny" target="_blank">👁</a>
           <br />
         </label>
-        <label>
+        <label className={styles.label}>
           <input type="checkbox" name="symbol_chicks" value="chicks" onChange={handleChecking} checked={ (formData.symbol_chicks === "chicks") } />
           &nbsp;Chicks
           &nbsp;<a href="/symbols/chicks" target="_blank">👁</a>
           <br />
         </label>
-        <label>
+        <label className={styles.label}>
           <input type="checkbox" name="symbol_cross" value="cross" onChange={handleChecking} checked={ (formData.symbol_cross === "cross") } />
           &nbsp;Cross
           &nbsp;<a href="/symbols/cross" target="_blank">👁</a>
           <br />
         </label>
-        <label>
+        <label className={styles.label}>
           <input type="checkbox" name="symbol_eggs" value="eggs" onChange={handleChecking} checked={ (formData.symbol_eggs === "eggs") } />
           &nbsp;Eggs
           &nbsp;<a href="/symbols/eggs" target="_blank">👁</a>
           <br />
         </label>
-        <label>
+        <label className={styles.label}>
           <input type="checkbox" name="symbol_lillies" value="lillies" onChange={handleChecking} checked={ (formData.symbol_lillies === "lillies") } />
           &nbsp;Lillies
           &nbsp;<a href="/symbols/lillies" target="_blank">👁</a>
           <br />
         </label>      
-        <button type="submit">Go!</button>
+        <button type="submit" className={styles.button}>Go!</button>
+        <br style={{ clear:"both" }} />
       </form>
       <div className={styles.poemContainer} dangerouslySetInnerHTML={{ __html: generation.poem }} />
     </div>
